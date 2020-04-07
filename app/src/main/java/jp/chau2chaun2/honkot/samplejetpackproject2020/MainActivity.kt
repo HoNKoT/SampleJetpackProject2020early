e41daf6335ceaabfa5d19345f2b7feda10865d57
Hiroki.H <hon87kot12.xp@gmail.com>
2020-04-07 15:09:18 +0900

    remove unused import

--- app/src/main/java/jp/chau2chaun2/honkot/samplejetpackproject2020/MainActivity.kt
+++ app/src/main/java/jp/chau2chaun2/honkot/samplejetpackproject2020/MainActivity.kt
@@ -3,12 +3,9 @@ package jp.chau2chaun2.honkot.samplejetpackproject2020
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
-import androidx.fragment.app.FragmentContainerView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import androidx.navigation.ui.setupActionBarWithNavController
-import jp.chau2chaun2.honkot.samplejetpackproject2020.ui.main.FirstFragment
 
 class MainActivity : AppCompatActivity() {
 
