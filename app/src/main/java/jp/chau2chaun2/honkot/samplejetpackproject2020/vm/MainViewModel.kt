fe9e7a2b17b9a129c792b104e8ec945e37ea8468
Hiroki.H <hon87kot12.xp@gmail.com>
2020-04-08 12:44:44 +0900

    add livedata to view model

--- app/src/main/java/jp/chau2chaun2/honkot/samplejetpackproject2020/vm/MainViewModel.kt
+++ app/src/main/java/jp/chau2chaun2/honkot/samplejetpackproject2020/vm/MainViewModel.kt
@@ -1,14 +1,34 @@
 package jp.chau2chaun2.honkot.samplejetpackproject2020.vm
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class MainViewModel(count: Int = 0) : ViewModel() {
-    var count: Int = 0
-        private set
+    /**
+     * For set count
+     */
+    private val mCount = MutableLiveData(0)
 
-    init {
-        this.count = count
+    /**
+     * For get count
+     */
+    val count: LiveData<Int> = mCount
+
+    /**
+     * Transform count to the message format automatically
+     */
+    val countMessage: LiveData<String> = Transformations.map(mCount) { count ->
+        "count number is $count"
     }
 
-    fun countUp() = count++
+    init {
+        mCount.value = count
+    }
+
+    fun countUp() {
+        val currentCount = mCount.value ?: throw Exception("Invalid _count value")
+        mCount.value = currentCount + 1
+    }
 }
