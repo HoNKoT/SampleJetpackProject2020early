aee72c236984bd7532cb513fd45c21a5b8795ca9
Hiroki.H <hon87kot12.xp@gmail.com>
2020-04-09 20:52:26 +0900

    remove test code

--- app/src/main/java/jp/chau2chaun2/honkot/samplejetpackproject2020/MainActivity.kt
+++ app/src/main/java/jp/chau2chaun2/honkot/samplejetpackproject2020/MainActivity.kt
@@ -1,25 +1,17 @@
 package jp.chau2chaun2.honkot.samplejetpackproject2020
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.android.support.DaggerAppCompatActivity
-import jp.chau2chaun2.honkot.samplejetpackproject2020.db.Repo
-import javax.inject.Inject
 
 class MainActivity : DaggerAppCompatActivity() {
-
-    @Inject
-    lateinit var repo: Repo
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-
-        Toast.makeText(this, repo.getName(), Toast.LENGTH_SHORT).show()
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
