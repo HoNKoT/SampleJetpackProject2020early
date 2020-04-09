c07d66cf5e33b56aa9cd8b0e4a60d7162a529fce
Hiroki.H <hon87kot12.xp@gmail.com>
2020-04-09 20:52:53 +0900

    add singleton to share viewmodel between fragments

--- app/src/main/java/jp/chau2chaun2/honkot/samplejetpackproject2020/di/ViewModelModule.kt
+++ app/src/main/java/jp/chau2chaun2/honkot/samplejetpackproject2020/di/ViewModelModule.kt
@@ -8,6 +8,7 @@ import dagger.Module
 import dagger.multibindings.IntoMap
 import jp.chau2chaun2.honkot.samplejetpackproject2020.CustomApplication
 import jp.chau2chaun2.honkot.samplejetpackproject2020.vm.MainViewModel
+import javax.inject.Singleton
 
 @Module
 interface ViewModelModule {
@@ -18,6 +19,7 @@ interface ViewModelModule {
     @Binds
     fun viewModelFactory(viewModelFactory: ViewModelFactory): ViewModelProvider.Factory
 
+    @Singleton
     @Binds
     @IntoMap
     @ViewModelKey(MainViewModel::class)
