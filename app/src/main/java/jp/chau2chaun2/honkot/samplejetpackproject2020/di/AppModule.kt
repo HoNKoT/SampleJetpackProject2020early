b7ab343946d998cd43de2114875f68a469a23f15
Hiroki Honda <hon87kot12.xp@gmail.com>
2020-11-13 17:21:33 +0900

    maintain di derectory

--- app/src/main/java/jp/chau2chaun2/honkot/samplejetpackproject2020/di/AppModule.kt
+++ app/src/main/java/jp/chau2chaun2/honkot/samplejetpackproject2020/di/AppModule.kt
@@ -1,15 +1,19 @@
 package jp.chau2chaun2.honkot.samplejetpackproject2020.di
 
+import android.content.Context
 import dagger.Module
 import dagger.Provides
-import jp.chau2chaun2.honkot.samplejetpackproject2020.CustomApplication
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
 import jp.chau2chaun2.honkot.samplejetpackproject2020.db.Repo
 import jp.chau2chaun2.honkot.samplejetpackproject2020.db.SampleRepo
 
 @Module
+@InstallIn(ApplicationComponent::class)
 class AppModule {
     @Provides
-    fun provideRepo(app: CustomApplication): Repo {
-        return SampleRepo(app)
+    fun provideRepo(@ApplicationContext context: Context): Repo {
+        return SampleRepo(context)
     }
 }
